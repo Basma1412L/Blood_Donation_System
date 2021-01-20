11ec0ddc3249bf5384582f65ace51e49e1c5c2e7
basmamohamed <basmammahdy@gmail.com>
2021-01-20 13:22:15 -0800

    Starter

---
+++ app.py
@@ -0,0 +1,267 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from models import *
+
+
+RESULTS_PER_PAGE = 10
+
+
+def paginate_results(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * RESULTS_PER_PAGE
+    end = start + RESULTS_PER_PAGE
+
+    results = [result.format() for result in selection]
+    current_results = results[start:end]
+    return current_results
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  setup_db(app)
+  CORS(app)
+
+  @app.after_request
+  def after_request(response):
+      response.headers.add(
+          'Access-Control-Allow-Headers',
+          'Content-Type, Authorization')
+      response.headers.add(
+          'Access-Control-Allow-Methods',
+          'GET, POST, PATCH, DELETE, OPTIONS')
+      return response
+
+  @app.route('/donors')
+  def retrive_donors():
+      donors = Donor.query.all()
+      if donors is None or len(donors) == 0:
+          abort(404)
+      length = len(donors)
+      return jsonify({
+          'success': True,
+          'donors': donors,
+          'total_donors': length
+      })
+
+  
+  @app.route('/donations')
+  def retrive_donations():
+      donations = Donation.query.all()
+      if donations is None or len(donations) == 0:
+          abort(404)
+      length = len(donations)
+      return jsonify({
+          'success': True,
+          'donations': donations,
+          'total_donations': length
+      })
+
+  @app.route('/donation_centers')
+  def retrive_donation_centers():
+      donation_centers = DonationCenter.query.all()
+      if donation_centers is None or len(donation_centers) == 0:
+          abort(404)
+      length = len(donation_centers)
+      return jsonify({
+          'success': True,
+          'donation_centers': donation_centers,
+          'total_donation_centers': length
+      })
+
+  @app.route('/appointments')
+  def retrive_appointments():
+      appointments = Appointment.query.all()
+      if appointments is None or len(appointments) == 0:
+          abort(404)
+      length = len(appointments)
+      return jsonify({
+          'success': True,
+          'appointments': appointments,
+          'total_appointments': length
+      })
+
+  @app.route('/appointments_donors')
+  def retrive_appointments_donors():
+      appointments_donors = AppointmentsDonors.query.all()
+      if appointments_donors is None or len(appointments_donors) == 0:
+          abort(404)
+      length = len(appointments_donors)
+      return jsonify({
+          'success': True,
+          'appointments_donors': appointments_donors,
+          'total_appointments_donors': length
+      })
+
+  @app.route('/DonationCenter', methods=['POST'])
+  def create_donation_center():
+      body = request.get_json()
+      new_name= body.get('name', None)
+      new_address = body.get('address', None)
+      try:
+          if new_name and new_address:
+              donation_center = DonationCenter(
+                  name=new_name,
+                  address=new_address)
+              donation_center.insert()
+
+              selection = DonationCenter.query.order_by(DonationCenter.id).all()
+              current_centers = paginate_result(request, selection)
+
+              return jsonify({
+                  'success': True,
+                  'created': donation_center.id,
+                  'donation_centers': current_centers,
+                  'donation_centers_count': len(DonationCenter.query.all())
+              })
+          else:
+              abort(404)
+      except Exception as error:
+          print("\nerror => {}\n".format(error))
+          abort(422)
+
+
+
+  
+  @app.route('/Appointment', methods=['POST'])
+  def create_appointment():
+      body = request.get_json()
+      new_donations_center= body.get('donations_center', None)
+      new_time = body.get('time', None)
+      new_donors_limit = body.get('donors_limit', None)
+      new_availibility= body.get('availibility', None)
+      try:
+          if new_donations_center and new_time and new_donors_limit and new_availibility:
+              appointment = Appointment(
+                  donations_center=new_donations_center,
+                  time=new_time,
+                  donors_limit=new_donors_limit,
+                  availibility=new_availibility)
+              appointment.insert()
+
+              selection = Appointment.query.order_by(Appointment.id).all()
+              current_appointments = paginate_result(request, selection)
+
+              return jsonify({
+                  'success': True,
+                  'created': appointment.id,
+                  'appointments': current_appointments,
+                  'appointments_count': len(Appointment.query.all())
+              })
+          else:
+              abort(404)
+      except Exception as error:
+          print("\nerror => {}\n".format(error))
+          abort(422)
+
+  
+  
+  @app.route('/Donor', methods=['POST'])
+  def create_donor():
+      body = request.get_json()
+      new_name= body.get('name', None)
+      new_age = body.get('age', None)
+      new_gender = body.get('gender', None)
+      new_phone= body.get('phone', None)
+      new_email= body.get('email', None)
+      new_blood_type= body.get('blood_type', None)
+      try:
+          if new_name and new_age and new_gender and new_phone and new_email and new_blood_type:
+              donor = Donor(
+                  name=new_name,
+                  age=new_age,
+                  gender=new_gender,
+                  phone=new_phone,
+                  email=new_email,
+                  blood_type=new_blood_type)
+              donor.insert()
+
+              selection = Donor.query.order_by(Donor.id).all()
+              current_donors= paginate_result(request, selection)
+
+              return jsonify({
+                  'success': True,
+                  'created': donor.id,
+                  'donors': current_donors,
+                  'donors_count': len(Donor.query.all())
+              })
+          else:
+              abort(404)
+      except Exception as error:
+          print("\nerror => {}\n".format(error))
+          abort(422)
+  return app
+
+
+
+
+  @app.route('/Donation', methods=['POST'])
+  def create_donation():
+      body = request.get_json()
+      new_donationCenter_id= body.get('donationCenter_id', None)
+      new_donor_id=body.get('donor_id', None)
+      new_blood_type= body.get('blood_type', None)
+      new_time = body.get('time', None)
+      try:
+          if new_donationCenter_id and new_donor_id and new_blood_type and new_time:
+              donation = Donation(
+                  donations_center_id=new_donationCenter_id,
+                  new_donor_id=donor_id,
+                  blood_type=new_blood_type,
+                  time=new_time)
+              donation.insert()
+
+              selection = Donation.query.order_by(Donation.id).all()
+              current_donations = paginate_result(request, selection)
+
+              return jsonify({
+                  'success': True,
+                  'created': donation.id,
+                  'donations': current_donations,
+                  'donations_count': len(Donation.query.all())
+              })
+          else:
+              abort(404)
+      except Exception as error:
+          print("\nerror => {}\n".format(error))
+          abort(422)
+
+
+
+
+  @app.route('/AppointmentsDonors', methods=['POST'])
+  def create_AppointmentsDonors():
+      body = request.get_json()
+      new_donor_id=body.get('donor_id', None)
+      new_appointment_id = body.get('appointment_id', None)
+      try:
+          if new_donor_id and new_appointment_id:
+              appointmentDonor = AppointmentsDonors(
+                  appointment_id=new_appointment_id,
+                  donor_id=new_donor_id
+                  )
+              appointmentDonor.insert()
+
+              selection = AppointmentsDonors.query.order_by(AppointmentsDonors.id).all()
+              current_appointmentsDonors = paginate_result(request, selection)
+
+              return jsonify({
+                  'success': True,
+                  'created': appointmentsDonor.id,
+                  'appointmentsDonors': current_appointmentsDonors,
+                  'appointmentsDonors_count': len(AppointmentsDonors.query.all())
+              })
+          else:
+              abort(404)
+      except Exception as error:
+          print("\nerror => {}\n".format(error))
+          abort(422)
+APP = create_app()
+
+if __name__ == '__main__':
+    APP.run(host='0.0.0.0', port=8080, debug=True)
+
+
+
+
---
+++ models.py
@@ -0,0 +1,199 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+from flask_migrate import Migrate
+
+
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', '')
+DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+DB_NAME = os.getenv('DB_NAME', 'blood_system')
+database_path = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    # db.create_all()
+
+
+'''
+Donor
+'''
+class Donor(db.Model):  
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  age = Column(Integer)
+  gender = Column(String)
+  address = Column(String)
+  phone = Column(String)
+  email = Column (String)
+  blood_type = Column (String)
+  
+  donation = db.relationship( 'Donation', backref=db.backref('donor', cascade='all, delete'))
+
+
+  def __init__(self, name, age, gender, address, phone, email, blood_type):
+    self.name = name
+    self.age = age
+    self.gender = gender
+    self.address = address
+    self.phone = phone
+    self.email = email
+    self.blood_type = blood_type
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'type': self.type,
+      'name' : self.name,
+      'age' :self.age,
+      'gender' : self.gender,
+      'address' : self.address,
+      'phone' : self.phone, 
+      'email' : self.email,
+      'blood_type' :self.blood_type
+    }
+
+
+'''
+Donation
+'''
+class Donation(db.Model):  
+
+  id = Column(Integer, primary_key=True)
+  blood_type = db.Column(db.String)
+  time=db.Column(db.DateTime())
+  donor_id = db.Column(db.Integer, db.ForeignKey('donor.id'))
+  donationCenter_id= db.Column(db.Integer, db.ForeignKey('donation_center.id'))
+
+
+  def __init__(self, donor_id, blood_type, time, donationCenter_id):
+    self.donor_id = donor_id
+    self.blood_type = blood_id
+    self.time = time
+    self.donationCenter_id = donationCenter_id
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'donor_id': self.donor_id,
+      'blood_type': self.blood_type,
+      'donationCenter_id': self.donationCenter_id,
+      'time': self.time
+    }
+
+
+'''
+Donation Center
+'''
+class DonationCenter(db.Model):  
+
+  id = Column(Integer, primary_key=True)
+  name = db.Column(db.String)
+  address = db.Column(db.String)
+
+  donations = db.relationship( 'Donation', backref=db.backref('donation_center', cascade='all, delete'))
+  appointments = db.relationship( 'Appointment', backref=db.backref('donation_center', cascade='all, delete'))
+
+  def __init__(self, name, address):
+    self.name=name,
+    self.address = address
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name' : self.name,
+      'address': self.address
+    }
+
+
+
+
+'''
+Appointment
+'''
+class Appointment(db.Model):  
+
+  id = Column(Integer, primary_key=True)
+  donations_center = db.Column(db.Integer, db.ForeignKey('donation_center.id'))
+  time=db.Column(db.DateTime())
+  donors_limit = db.Column(db.Integer)
+  availibility = db.Column(db.Boolean, default=True)
+
+
+  def __init__(self, address, donations_ids, donor_id):
+    self.address = address
+    self.donations_ids = donations_ids
+    self.donor_id = donor_id
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'donations_center': self.donations_center,
+      'time': self.time
+    }
+
+
+class AppointmentsDonors(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    donor_id = db.Column(db.Integer, db.ForeignKey('donor.id'))
+    appointment_id = db.Column(db.Integer, db.ForeignKey('appointment.id'))
+
+    donor = db.relationship( Donor, backref=db.backref('appointments_donors', cascade='all, delete'))
+    appointment = db.relationship( Appointment, backref=db.backref('appointments_donors', cascade='all, delete'))
