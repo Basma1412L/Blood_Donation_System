8546051f1af94f0dce0240ca3c21a0bbada09881
basmamohamed <basmammahdy@gmail.com>
2021-01-22 01:19:44 -0800

    add migrations

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+SECRET_KEY = os.urandom(32)
+
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Database connection string
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', '')
+DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+DB_NAME = os.getenv('DB_NAME', 'blood_system')
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+
+# Supress warning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
