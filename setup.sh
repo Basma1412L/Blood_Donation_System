4512d76438d3fb4bfc0d811e322b5333b1ae1607
basmamohamed <basmammahdy@gmail.com>
2021-01-22 14:47:07 -0800

    tokens

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
 
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', '')
+DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+DB_NAME = os.getenv('DB_NAME', 'blood_system')
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+AUTH0_DOMAIN = 'dev-16nawflo.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'blood_donation'
