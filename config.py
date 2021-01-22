43d36b73f99707ec01870b9c252f2236793cc6f3
basmamohamed <basmammahdy@gmail.com>
2021-01-22 01:41:53 -0800

    t

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
 DB_USER = os.getenv('DB_USER', '')
 DB_PASSWORD = os.getenv('DB_PASSWORD', '')
 DB_NAME = os.getenv('DB_NAME', 'blood_system')
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
-
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost:5432/wmd'
 # Supress warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
